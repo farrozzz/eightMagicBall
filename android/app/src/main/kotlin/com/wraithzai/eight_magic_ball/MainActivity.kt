98fa4bd6f5480d596eed7f00a7f83d535225b921
Farros Zain <zain.farros@gmail.com>
2023-04-29 22:50:46 +0700

    made my eight magic ball to answer your questions

---
+++ android/app/src/main/kotlin/com/wraithzai/eight_magic_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wraithzai.eight_magic_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main(){
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+      title: Text ("Eight Magic Ball"),
+      backgroundColor: Colors.black87,
+        ),
+    body: MagicBallPage(),
+  ),
+  ),
+  );
+}
+
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({Key? key}) : super(key: key);
+
+  @override
+  State<MagicBallPage> createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int ballNumber = 1;
+  void changeBallNumber(){
+    setState(() {
+      ballNumber = Random().nextInt(5)+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: TextButton(
+          onPressed: changeBallNumber,
+          child: Image(
+              image: AssetImage("assets/images/ball$ballNumber.png"),
+       ),
+        ),
+      ),
+    );
+  }
+}
+
